2cdf3e90321271596139482de1e5d863c6c3737d
andrey_solovev (Andrey) <fr0ga@yandex.ru>
2016-12-14 19:39:51 +0300

    added answers here as comments

--- src/CoConstructor.java
+++ src/CoConstructor.java
@@ -7,12 +7,15 @@ public class CoConstructor {
 
         A a = new A(1,2,3);
         System.out.println();
+        // a1 a2 a3
 
         B b = new B(4,5,6);
         System.out.println();
+        // a1 a2 a3 b3
 
         C c = new C();
         System.out.println();
+        // a1 b1 c1
 
         C c3 = new C(1,2,3);
         // a1 a2 a3 b3 c3
